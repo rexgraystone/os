687e548ecb56c4cd3c01ba7f063615ebdf21d579
Raghav Nanjappan <r6nanjappan@gmail.com>
2022-11-04 21:03:18 +0530

    Update programs

--- M1_LSearch.c
+++ M1_LSearch.c
@@ -4,21 +4,39 @@
 
 #include <stdio.h>
 
-int main() {
-    int n, s;
-    int arr[n];
-    printf("Enter the number of elements in the array: ");
-    scanf("%d", &n);
-    for(int i = 0; i < n; i++) {
-        printf("\nEnter number %d: ", i+1);
-        scanf("%d", &arr[i]);
+int enterArr(int n, int arr[n]) { // enter the elements of the array
+    printf("Enter the elements for an array of size %d - \n\n", n); // input the elements of the array
+    for(int i = 0; i < n; i++) { // for all the elements in the array
+        printf("\tEnter the element at position %d: ", i); // input the element
+        scanf("%d", &arr[i]); // store the element in the array
     }
-    printf("\nEnter a number to search in the array: ");
-    scanf("%d", &s);
-    for(int i = 0; i < n; i++) {
-        if(arr[i] == s) {
-            printf("\nElement %d found at array position %d.", s, i);
-            break;
+    return n; // return the number of elements in the array
+}
+
+int linearSearch(int arr[], int n, int x) { // n = number of elements in the array, x = number to be searched
+    for(int i = 0; i < n; i++) { // loop through the array
+        if(arr[i] == x) { // if the element is the number to be searched
+            return i; // return the index of the element
         }
     }
+    return -1; // if the number is not found
+}
+
+int main() { 
+    int n; // n = number of elements in the array
+    printf("Enter the number of elements in the array: "); // input the number of elements in the array
+    scanf("%d", &n); // store the number of elements in the array in n
+    int arr[n]; // arr = array
+    enterArr(n, arr); // call the function to enter the elements of the array
+    int x; // x = number to be searched
+    printf("Enter the number to be searched: "); // input the number to be searched
+    scanf("%d", &x); // store the number to be searched in x
+    int result = linearSearch(arr, n, x); // call the function to search for the number
+    if(result == -1) { // if the number is not found
+        printf("The number %d is not found in the array.\n", x); // print the number is not found
+    } 
+    else { // if the number is found
+        printf("The number %d is found at position %d in the array.\n", x, result); // print the number is found
+    }
+    return 0;
 }
--- M2_BSearch.c
+++ M2_BSearch.c
@@ -4,32 +4,33 @@
 
 #include <stdio.h>
 
-int BSearch(int arr[], int l, int h, int n) {
-    if (h >= l) {
-        int m = l + (h - l)/2;
-        if (arr[m] == n) {
-            return m;
+int binarySearch(int arr[], int l, int h, int n) { // l = low, h = high, n = number to be searched
+    if (h >= l) { // base case
+        int mid = l + (h - l)/2; // calculate the middle index
+        if (arr[mid] == n) { // if the middle element is the number to be searched
+            return mid; // return the middle index
         }
-        if (arr[m] > n) {
-            return BSearch(arr, l, m-1, n);
+        if (arr[mid] > n) { // if the middle element is greater than the number to be searched
+            return binarySearch(arr, l, (mid - 1), n); // search in the left subarray
         }    
-        return BSearch(arr, m+1, h, n);
+        return binarySearch(arr, (mid + 1), h, n); // search in the right subarray
     }
-    return -1;
+    return -1; // if the number is not found
 }
 
 int main(void) {
-    int arr[] = {1, 4, 7, 9, 16, 56, 70};
-    int h = 7;
-    int n;
-    printf("Enter a number: \n");
-    scanf("%d", &n);
-    int i = BSearch(arr, 0, h-1, n);
-    if(i == -1 ) {
-        printf("Element %d not found in the array.\n", n);
+    int arr[] = {1, 4, 7, 9, 16, 56, 70}; // array to be searched
+    int l =0; // low index
+    int h = 7; // high index
+    int n; // n = number to be searched
+    printf("Enter a number: \n"); // input the number to be searched
+    scanf("%d", &n); // store the number to be searched in n
+    int i = binarySearch(arr, l, (h - 1), n); // call the function
+    if(i == -1 ) { // if the number is not found
+        printf("Element %d not found in the array.\n", n); // print the number is not found
     }
-    else {
-        printf("Element %d found at index: %d.\n", n, i);
+    else { // if the number is found
+        printf("Element %d found at index: %d.\n", n, i); // print the number is found
     }
     return 0;
 }
